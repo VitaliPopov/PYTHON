d52bc382ec8f723d56998d3b2fcff52b829cfcb8
Vitalii Popov <vetlo@yandex.ru>
2023-06-10 16:14:03 +0300

    Истинность квадрата числа

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test02/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+a = 25; b = 5 -> да
+a = 2 b = 10 -> нет
+a = 9; b = -3 -> да
+a = -3 b = 9 -> нет*/
+
+Console.Write("Enter number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number b: ");
+int b = int.Parse(Console.ReadLine());
+if (a * a == b) {
+    Console.WriteLine($"-> yes {a*a == b}");
+    } else Console.WriteLine($"-> no {a*a == b}");
