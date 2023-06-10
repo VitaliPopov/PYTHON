0716f2de0be3641370c2adc78df41fee4809789a
Vitalii Popov <vetlo@yandex.ru>
2023-06-10 16:12:47 +0300

    Квадрат числа

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter number a:");
+int a = int.Parse(Console.ReadLine());
+//первый способ
+int result1 = a * a;
+Console.WriteLine($"Способ 1 -> Квадрат числа {a} равен {result1}");
+//второй способ
+int result2 = Convert.ToInt32(Math.Pow(a, 2));
+Console.WriteLine($"Способ 2 -> Квадрат числа {a} равен {result2}");
