0ff3624bfd3e409d8cda6e0edb823bfbeaeba9a4
Vitalii Popov <vetlo@yandex.ru>
2023-06-10 16:17:02 +0300

    Последняя цифра числа

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test05/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает **последнюю** цифру этого числа.
+    
+    456 -> 6
+    
+    782 -> 2
+    
+    918 -> 8*/
+
+Console.Write("Enter number N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Последняя цифра числа равна");
+Console.Write(N % 100);
+Console.WriteLine(" результат деления ");
+Console.WriteLine(N / 10);
