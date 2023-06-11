d15da8db5ba76cec1c8fce21a17bd10d1f63362a
Vitalii Popov <vetlo@yandex.ru>
2023-06-11 14:25:22 +0300

    Удаляет вторую цифру из трехзначного

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test11/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+int num = new Random().Next(100, 1000);
+int num1 = (num / 100) * 10;
+int num2 = num % 10;
+int num3 = num1 + num2;
+Console.WriteLine($"Двузначное от трехзначного {num} -> {num3}");
