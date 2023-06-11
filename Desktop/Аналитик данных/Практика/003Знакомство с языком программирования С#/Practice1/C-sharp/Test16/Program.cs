e4253da9824a62fe3c58cf3f9cc68f0a06632c42
Vitalii Popov <vetlo@yandex.ru>
2023-06-11 15:20:36 +0300

    Квадрат из пары чисел

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test16/Program.cs
@@ -0,0 +1,15 @@
+﻿// 16 Напишите программу, которая принимает на вход два числа и проверяет,
+    // является ли одно число квадратом другого.
+
+    // 5, 25  ->  да
+    // -4, 16  ->  да
+    // 25, 5  ->  да
+    // 8,9  ->  нет
+
+Console.Write("Enter number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number b: ");
+int b = int.Parse(Console.ReadLine());
+if (a * a == b || b * b == a){
+    Console.WriteLine($"{a}, {b}-> yes");
+    } else Console.WriteLine($"{a}, {b}-> no");
