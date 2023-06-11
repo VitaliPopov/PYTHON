b02cb6d526c4f0a9cba534f8be18302e2ae666cb
Vitalii Popov <vetlo@yandex.ru>
2023-06-11 14:49:34 +0300

    Кратность чисел

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test12/Program.cs
@@ -0,0 +1,15 @@
+﻿/*12 Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если второе число не кратно числу первому,
+то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4  -> кратно
+*/
+
+Console.Write("Enter number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number b: ");
+int b = int.Parse(Console.ReadLine());
+if (a % b ==0){
+    Console.Write("Кратно");
+    } else Console.WriteLine($"-> no {a % b}");
