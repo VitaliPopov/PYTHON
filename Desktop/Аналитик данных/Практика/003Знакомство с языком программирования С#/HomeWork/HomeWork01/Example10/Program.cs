4a3378f8c337dd52f01383fb49ec7fc519acb4a6
Vitalii Popov <vetlo@yandex.ru>
2023-06-12 11:12:34 +0300

    Задача №10

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/HomeWork/HomeWork01/Example10/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write($"{N} -> ");
+int num1 = (N / 10);
+int num2 = (num1 % 10);
+Console.WriteLine($"{num2}");
