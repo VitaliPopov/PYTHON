b3f38515a2ca9c4ab3424fc2657b52934be502bd
Vitalii Popov <vetlo@yandex.ru>
2023-06-12 15:13:12 +0300

    Задача №13

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/HomeWork/HomeWork01/Example13/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write($"{N} -> ");
+
+int num1 = 0;
+if (N / 100 > 0){
+     while (N > 99){
+          num1 = (N % 10);
+          N = N / 10;
+          }
+     Console.Write($"{num1}");
+    }
+else Console.WriteLine("there is no third number");
