ec4a71fac7186a41471aa78e8602e55ed3e88718
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 20:13:24 +0300

    Задача №18.1

--- Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test18/Program.cs
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test18/Program.cs
@@ -4,7 +4,7 @@
 
 
 Console.Clear();
-Console.WriteLine("Enter number: ");
+Console.WriteLine("Enter quarter: ");
 int N = int.Parse(Console.ReadLine());
 if (N==1){
     Console.WriteLine("X>0 and Y>0");
@@ -15,3 +15,28 @@ if (N==1){
 } else if (N==4){
     Console.WriteLine("X<0 and Y>0");
 } else Console.WriteLine("Вне плоскости");
+Console.WriteLine();
+
+switch (N)
+{
+    case 1:{
+    Console.WriteLine("X>0 and Y>0");
+    break;
+    }
+    case 2:{
+    Console.WriteLine("X>0 and Y<0");
+    break;
+    }
+    case 3:{
+    Console.WriteLine("X<0 and Y<0");
+    break;
+    }
+    case 4:{
+    Console.WriteLine("X<0 and Y>0");
+    break;
+    }
+    default:{
+        Console.WriteLine("Введена неправильная четверть");
+        break;
+        }
+}       
