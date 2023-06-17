b9ff035af9a2fc02ad9a5740c072d37fee783d3d
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 15:00:44 +0300

    Задача №19

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test19/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number: ");
+int Y = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+double s = Math.Sqrt((Math.Pow(X-M,2))+(Math.Pow(Y-N,2)));
+Console.WriteLine($"расстояние между точками равно {s}");
