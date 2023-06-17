875a99ae45245b889e5f0b26af62485530115f27
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 15:19:51 +0300

    Задача №22

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу квадратов чисел от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+
+// 2 -> 1,4
+Console.Clear();
+Console.Write("Enter number N: ");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+while (a <N)
+{
+int s = Convert.ToInt32(Math.Pow(a,2));
+Console.Write($"{s}, ");
+a++;
+}
+Console.Write($"{N*N}. ");
