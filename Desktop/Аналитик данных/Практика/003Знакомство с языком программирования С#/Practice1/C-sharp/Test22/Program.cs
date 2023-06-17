12d1ed65106b7fa277afeabec6aad57678779ab5
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 20:04:55 +0300

    Задача №22.1

--- Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test22/Program.cs
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test22/Program.cs
@@ -7,6 +7,19 @@
 Console.Clear();
 Console.Write("Enter number N: ");
 int N = int.Parse(Console.ReadLine());
+int[] result = new int[N];
+
+for (int i=1; i<=N; i++)
+    result[i-1] = i*i;
+
+for (int i=0; i<N; i++)
+    Console.WriteLine($"{result[i]} ");
+Console.WriteLine();  
+
+for (int i=1; i<=N; i++)
+    Console.WriteLine($"{i} в квадрате = {Math.Pow(i, 2)}");
+
+
 int a = 1;
 while (a <N)
 {
