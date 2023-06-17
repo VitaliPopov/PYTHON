3c5f8275bf9bd887405cc64724b1ec449e386162
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 19:34:34 +0300

    Задача17.1

--- Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test17/Program.cs
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test17/Program.cs
@@ -5,5 +5,17 @@
 
 
 Console.Clear();
-Console.WriteLine("Enter number: ");
-int N = int.Parse(Console.ReadLine());
+Console.Write("Enter X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Enter Y: ");
+int Y = int.Parse(Console.ReadLine());
+ if (X>0 && Y>0){
+    Console.WriteLine("First quarter");
+ }
+ else if (X>0 && Y<0){
+    Console.WriteLine("Second quarter");
+ }
+ else if (X<0 && Y<0){
+    Console.WriteLine("Third quarter");
+ }
+ else Console.WriteLine("Fourth quarter");
