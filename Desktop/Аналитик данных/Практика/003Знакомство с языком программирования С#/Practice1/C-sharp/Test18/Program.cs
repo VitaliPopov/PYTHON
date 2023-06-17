5ffd8ef9f02c40eb2498fb6cb1e712205c9c5f35
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 14:31:32 +0300

    Задача 18

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+if (N==1){
+    Console.WriteLine("X>0 and Y>0");
+} else if (N==2){
+    Console.WriteLine("X>0 and Y<0");
+} else if (N==3){
+   Console.WriteLine("X<0 and Y<0"); 
+} else if (N==4){
+    Console.WriteLine("X<0 and Y>0");
+} else Console.WriteLine("Вне плоскости");
