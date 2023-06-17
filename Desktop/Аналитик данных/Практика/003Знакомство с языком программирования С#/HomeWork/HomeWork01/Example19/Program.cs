2d387df326579860d8b118b097cafbc8d72f1f75
Vitalii Popov <vetlo@yandex.ru>
2023-06-17 20:45:59 +0300

    Пятизначный Палиндром

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/HomeWork/HomeWork01/Example19/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+if ((N/10000) == N%10 && (N/1000)%10 == (N/10)%10){
+    Console.WriteLine($"{N} - это палиндром!");
+}
+else Console.WriteLine($"{N} - это НЕ палиндром!");
