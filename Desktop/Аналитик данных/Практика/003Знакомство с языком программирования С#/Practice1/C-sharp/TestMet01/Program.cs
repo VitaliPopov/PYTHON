e9971a0e4522d69260afca00b55fcd53b3221cb8
Vitalii Popov <vetlo@yandex.ru>
2023-06-18 15:34:48 +0300

    Количество цифр в числе

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/TestMet01/Program.cs
@@ -0,0 +1,20 @@
+﻿// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+Console.Write("Enter number: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество = {CountNumbers(s)}");
+int CountNumbers(int n)
+{
+    int count = 0;
+    while (n>0)
+    {
+        n /= 10; //<=> n = n/10
+        count++;
+    }
+    return count;
+}
