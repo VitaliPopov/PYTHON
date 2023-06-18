db2e3e0b81d0e5221988dfd2ffb28bda0d44146b
Vitalii Popov <vetlo@yandex.ru>
2023-06-18 15:38:05 +0300

    Рандомный массив от 0 до 1

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/TestMet03/Program.cs
@@ -0,0 +1,18 @@
+﻿//30 Напишите программу, которая выводит массив из 8 элементов, 
+//заполненный нулями и единицами в случайном порядке.
+//int[]
+// //[1,0,1,1,0,1,0,0]
+// int a = new Random().Next(2, 7);
+// int b = new Random().Next(2);
+// Console.WriteLine($"a = {a},b = {b}");
+
+int [] GetArray(int size){
+    int[] result = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        result[i] = new Random().Next(0,2);
+    }
+    return result;
+}
+int[] array = GetArray(8);
+Console.WriteLine($"[{String.Join("; ", array)}]");
