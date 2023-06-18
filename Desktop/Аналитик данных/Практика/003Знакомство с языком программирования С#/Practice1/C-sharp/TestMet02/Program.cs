91cd4fe789637682a25ad9bdb095659ff668e840
Vitalii Popov <vetlo@yandex.ru>
2023-06-18 15:36:55 +0300

    Произведение цифр числа

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/TestMet02/Program.cs
@@ -0,0 +1,19 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+Console.Clear();
+Console.Write("Enter number: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество = {GetMultiPly(s)}");
+int GetMultiPly(int n)
+{
+    int result = 1;
+    for (int i =2; i <= n;  i++)
+    {
+        result *=i;
+    }
+    return result;
+}
