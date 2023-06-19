dc69e3a63ba0a1b7131578211984167b6c1d0e01
Vitalii Popov <vetlo@yandex.ru>
2023-06-19 11:18:28 +0300

    Возводим в степень

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/HomeWork/HomeWork01/Example25/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.Clear();
+Console.WriteLine("Enter X:");
+int X = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Y:");
+int Y = int.Parse(Console.ReadLine());
+Console.WriteLine($"{X}^{Y} = {result}");
+int result(int s)
+{
+    int newresult = X;
+    {
+        for (int s=1; s<=Y; s++);
+        newresult = newresult*X;
+    }
+    return newresult;
+}
