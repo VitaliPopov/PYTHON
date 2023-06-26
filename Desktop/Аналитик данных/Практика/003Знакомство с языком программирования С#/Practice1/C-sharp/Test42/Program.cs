4632cfbb991219ce013270105320de63a22cf676
Vitalii Popov <vetlo@yandex.ru>
2023-06-26 11:03:25 +0300

    Задача 42. Десятичное число в двоичное

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test42/Program.cs
@@ -0,0 +1,22 @@
+﻿// **Задача 42:**Напишите программу,
+//  которая будет преобразовывать десятичное число в двоичное.
+
+// 45 -> 101101
+
+// 3 -> 11
+
+// 2 -> 10
+Console.Clear();
+int Number(int a){
+string result = " ";
+
+
+while (a > 0)
+{
+    result= Convert.ToString(a % 2) + result;
+    a= a/2;
+}
+return int.Parse(result);
+}
+int a= Number(45);
+Console.WriteLine(a);
