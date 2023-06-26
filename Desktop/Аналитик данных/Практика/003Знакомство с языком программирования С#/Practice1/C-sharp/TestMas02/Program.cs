5891bc9d352791873160a79beca985719e42c9d8
Vitalii Popov <vetlo@yandex.ru>
2023-06-26 10:58:30 +0300

    Задача 33. Присутствие числа в массиве

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/TestMas02/Program.cs
@@ -0,0 +1,19 @@
+﻿/* **Задача 33:**Задайте массив.Напишите программу, 
+которая определяет, присутствует ли заданное число в массиве.
+
+4; массив[6, 7, 19, 345, 3]->нет
+3; массив[6, 7, 19, 345, 3]->да*/
+
+int [] GetArray(int size){
+    int[] result= new int[size];
+    for (int i=0; i< size; i++)
+    {
+        int N = int.Parse(Console.ReadLine());
+        result[i]=N;
+
+    }
+    return result;
+}
+int [] GetNewArray(int newsize){
+    
+}
