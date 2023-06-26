270626c09f76f64e5a25367b1c0e48b895cd2595
Vitalii Popov <vetlo@yandex.ru>
2023-06-26 10:57:20 +0300

    Задача 32. Замена полож элем массива на отриц

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/TestMas01/Program.cs
@@ -0,0 +1,30 @@
+﻿/* **Задача 32:**Напишите программу замена ѝлементов маѝѝива: 
+положительные ѝлементы замените на ѝоответѝтвующие отрицательные, и наоборот.
+
+[-4, -8, 8, 2] -> [4, 8, -8, -2]  String.Join(", ", array)*/
+
+Console.Clear();
+int[] startArray = GetArray(6, -10, 10);
+int[] startArray2 = GetArray(12, -100, 150);
+Console.WriteLine($"startArray -> {String.Join(" ", startArray)}");
+Console.WriteLine($"startArray -> {String.Join(" ", InversArray(startArray))}");
+Console.WriteLine($"startArray -> {String.Join(" ", startArray2)}");
+Console.WriteLine($"startArray -> {String.Join(" ", InversArray(startArray2))}");
+
+
+int[] GetArray(int size, int minel, int maxel){
+    int [] result = new int [size];
+    for (int i=0; i< size; i++)
+    {
+        result[i]=new Random().Next(minel, maxel+1);
+    }
+        return result;
+}
+int[] InversArray(int [] array){
+    for (int i=0; i< array.Length; i++)
+    {
+        array[i] *=-1;
+    }
+    return array;
+}
+//Console.WriteLine(GetArray);
