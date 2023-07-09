f4908eea8981e1855231a9c2a07605f290db983f
Vitalii Popov <vetlo@yandex.ru>
2023-07-09 12:29:43 +0300

    Задача 67. На входе число- на выходе сумма цифр

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test67/Program.cs
@@ -0,0 +1,20 @@
+﻿/***Задача 67:** Напишите программу, 
+которая будет принимать на вход число и возвращать сумму его цифр.
+
+453 -> 12
+
+45 -> 9*/
+
+using System;
+using static System.Console;
+Clear();
+Write("Enter number: ");
+int s = int.Parse(ReadLine());
+WriteLine(GetSum(s));
+
+int GetSum(int s)
+{
+    if (s==0)
+    return s;
+    return (s%10 + GetSum(s/10));
+}
