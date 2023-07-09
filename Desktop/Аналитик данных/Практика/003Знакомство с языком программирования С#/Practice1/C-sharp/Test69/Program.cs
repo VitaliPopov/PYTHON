7ef5c3209d1b42cdf61d8a2cd03952f0f3881565
Vitalii Popov <vetlo@yandex.ru>
2023-07-09 12:32:08 +0300

    Задача 69. Возведение числа в степень

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/Practice1/C-sharp/Test69/Program.cs
@@ -0,0 +1,25 @@
+﻿/**Задача 69:** Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+A = 3; B = 5 -> 243 (3⁵)
+
+A = 2; B = 3 -> 8*/
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите первое число N: ");
+int n=int.Parse(ReadLine());
+
+Write("Введите второе число M: ");
+int m=int.Parse(ReadLine());
+
+WriteLine($"{n}^{m} = {Power(n,m)}");
+int Power(int n, int m)
+{
+    if(m==1)
+    
+        return n;
+        //return (n * Power(n,m-1));
+    
+    return (n * Power(n,m-1));
+}
