a927ee128b197d8f085f3d342f163a52a37397a7
Vitalii Popov <vetlo@yandex.ru>
2023-07-10 13:06:31 +0300

    Задача 66. Сумма чисел от Н до М

---
+++ Desktop/Аналитик данных/Практика/003Знакомство с языком программирования С#/HomeWork/HomeWork01/Example66/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите первое число N: ");
+int n=int.Parse(ReadLine());
+
+Write("Введите второе число M: ");
+int m=int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(n,m));
+
+WriteLine(GetSum(n,m));
+
+string PrintNumbers(int n, int m)
+{
+    if (m==n)
+    {
+        return n.ToString();
+    }
+    string s = PrintNumbers(n, m-1) + ", "+ m.ToString();
+    return s;
+}
+
+int GetSum(int n, int m)
+{
+    int sum=n;
+    if (sum==m)
+    return sum;
+    return (sum+GetSum(sum+1,m));
+}
